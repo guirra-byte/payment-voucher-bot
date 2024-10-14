dc8c7478a7b3a8c39fa7d50f88849e1ee8e87f70
guirra-byte <guirramatheus1@gmail.com>
2024-10-14 09:37:32 -0300

    feat: add solution to grow course stage automatically and get pending payments to notify formed students (shapers)

--- src/shared/prisma/seed.js
+++ src/shared/prisma/seed.js
@@ -8,39 +8,74 @@ const { formatDate } = require("../../helpers/format-date");
   const stages = [
     { name: "RECRUIT", amount: 80 },
     { name: "INTERN", amount: 60 },
-    { name: "SHAPER", amount: 40 },
+    { name: "SHAPER", amount: 40 }
   ];
 
   const period = formatDate(new Date());
   if (period) {
     const [month, year] = period.split("/");
     if (month === "set") {
+      const stages = await client.stage.findMany();
+      const filterByPendingPayments = await client.payer.findMany({
+        where: {
+          payments: {
+            none: {
+              status: "PAID"
+            }
+          }
+        },
+        include: {
+          payments: {
+            where: {
+              status: {
+                not: "PAID"
+              }
+            }
+          }
+        }
+      });
+
+      const growRecruit = stages.find((stage) => stage.name === "INTERN");
+      await client.payer.updateMany({
+        where: { stage: "RECRUIT" },
+        data: { stageId: growRecruit.id }
+      });
+
+      const growIntern = stages.find((stage) => stage.name === "SHAPER");
+      await client.payer.updateMany({
+        where: { stage: "INTERN" },
+        data: { stageId: growIntern.id }
+      });
+
+      const pendingShaperPayments = filterByPendingPayments.filter(
+        (payment) => payment.stageId === growIntern.id
+      );
+
       // Update all Lifeshapers stage in course;
-      // Remember, the last class (Shapers) get out in application (Remove all in database);
+      // Remember, the last class (Shapers) get out in application (Remove all in database) after all pending payments was payed;
       // Send message to require unpaid billings.
-      growStage();
     }
 
     for (let stage of stages) {
       const stageAlreadyNoted = await client.stage.findUnique({
         where: {
-          stage: stage.name,
-        },
+          stage: stage.name
+        }
       });
 
       if (!stageAlreadyNoted) {
         await client.stage.create({
           id: nanoid(),
           name: stage.name,
-          billingAmount: stage.amount,
+          billingAmount: stage.amount
         });
       }
 
       const periodAlreadyNoted = await client.billing.findUnique({
         where: {
           period,
-          stage: stage.name,
-        },
+          stage: stage.name
+        }
       });
 
       if (!periodAlreadyNoted) {
@@ -49,8 +84,8 @@ const { formatDate } = require("../../helpers/format-date");
             id: nanoid(),
             period,
             stage: stage.name,
-            amount: stage.amount,
-          },
+            amount: stage.amount
+          }
         });
       }
     }
